42496422b7617850b16ea56b830af1b39755197d
João Alcantara <joao.alcantara@fabricainfo.com>
2024-10-09 02:10:01 -0300

    Create project

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -7,6 +7,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Project extends Model
 {
-    /** @use HasFactory<\Database\Factories\ProjectFactory> */
     use HasFactory;
+
+    public function casts()
+    {
+        return [
+            'tech_stack' => 'array',
+        ];
+    }
 }
--- database/migrations/2024_10_09_043841_create_projects_table.php
+++ database/migrations/2024_10_09_043841_create_projects_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\User;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -13,6 +14,12 @@ return new class extends Migration
     {
         Schema::create('projects', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->text('description');
+            $table->dateTime('ends_at');
+            $table->string('status')->default('open');
+            $table->json('tech_stack');
+            $table->foreignIdFor(User::class, 'created_by')->constrained('users');
             $table->timestamps();
         });
     }
