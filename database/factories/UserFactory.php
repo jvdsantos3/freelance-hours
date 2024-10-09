2ed346c15e9ac253e0d5b2a818989a2617e6f9f5
João Alcantara <joao.alcantara@fabricainfo.com>
2024-10-09 01:39:25 -0300

    initial commit

---
+++ database/factories/UserFactory.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
+ */
+class UserFactory extends Factory
+{
+    public function definition(): array
+    {
+        return [
+            'name' => fake()->name(),
+            'email' => fake()->unique()->safeEmail(),
+            'rating' => fake()->randomElement([1, 2, 3, 4, 5]),
+            'avatar' => 'https://avatar.iran.liara.run/public',
+        ];
+    }
+}
